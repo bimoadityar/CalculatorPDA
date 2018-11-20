b6cfa3b40d79f6207a9f21a430d98f34c2367058
hafidhrendyanto <44597426+hafidhrendyanto@users.noreply.github.com>
2018-11-20 16:28:13 +0700

    Add files via upload

--- calc.c
+++ calc.c
@@ -9,7 +9,7 @@ Token readToken() {
 	char S[100];
 	Token Temp;
 	scanf("%s", S);
-	if ((strcmp(S, "+") != 0) && (strcmp(S, "-") != 0) && (strcmp(S, "*") != 0)	&& (strcmp(S, "/") != 0) && (strcmp(S, "^") != 0) && (strcmp(S, "=") != 0)) {
+	if ((strcmp(S, "+") != 0) && (strcmp(S, "-") != 0) && (strcmp(S, "*") != 0)	&& (strcmp(S, "/") != 0) && (strcmp(S, "^") != 0) && (strcmp(S, "=") != 0) && (strcmp(S, "(") != 0) && (strcmp(S, ")") != 0)) {
 		op(Temp) = 'n';
 		val(Temp) = atof(S);
 	} else {
@@ -37,6 +37,97 @@ int lastPrecedence;
 double a, b, temp;
 char opr;
 
+Token computeGan() {
+	int lastPrecedence;
+	double a, b, temp;
+	char opr;
+	Token currTkn, tknHasil;
+	do {
+		currTkn = readToken();
+		if (op(currTkn) == 'n') {
+			if (op(top(TknStck)) == 'n') {
+				b = val(PopStack(&TknStck));
+				val(currTkn) *= b;
+			}
+			PushStack(&TknStck, currTkn); 
+		} else if (op(currTkn) == '(') {
+			PushStack(&TknStck, currTkn);
+			computeGan();
+			PopStack(&TknStck);
+			tknHasil = PopStack(&TknStck);
+			PopStack(&TknStck);
+			if (op(top(TknStck)) == 'n') {
+				b = val(PopStack(&TknStck));
+				val(tknHasil) *= b;
+			} 
+			PushStack(&TknStck, tknHasil);
+		} else {
+			if ((getPrecedence(currTkn) > lastPrecedence) || (lastPrecedence == 3 && getPrecedence(currTkn) == 3)) {
+				PushStack(&TknStck, currTkn);
+				lastPrecedence = getPrecedence(currTkn);
+			} else {
+				a = val(PopStack(&TknStck));
+				opr = op(PopStack(&TknStck));
+				b = val(PopStack(&TknStck));
+				if (opr == '+') {
+					op(tknHasil) = 'n';
+					val(tknHasil) = b + a;
+				} else if (opr == '-') {
+					op(tknHasil) = 'n';
+					val(tknHasil) = b - a;
+				} else if (opr == '*') {
+					op(tknHasil) = 'n';
+					val(tknHasil) = b * a;
+				} else if (opr == '/') {
+					op(tknHasil) = 'n';
+					val(tknHasil) = b / a;
+				} else if (opr == '^') {
+					op(tknHasil) = 'n';
+					val(tknHasil) = pow(b, a);
+				} else {
+					printf("Token Salah!\n");
+				}
+				if (IsEmpty(TknStck)) {
+					PushStack(&TknStck, tknHasil);
+					PushStack(&TknStck, currTkn);
+					lastPrecedence = getPrecedence(currTkn);
+				} else {
+					while (op(top(TknStck)) != '(') {
+						opr = op(PopStack(&TknStck));
+						b = val(PopStack(&TknStck));
+						if (opr == '+') {
+							op(tknHasil) = 'n';
+							temp = b + val(tknHasil);
+							val(tknHasil) = temp;
+						} else if (opr == '-') {
+							op(tknHasil) = 'n';
+							temp = b - val(tknHasil);
+							val(tknHasil) = temp;
+						} else if (opr == '*') {
+							op(tknHasil) = 'n';
+							temp = b * val(tknHasil);
+							val(tknHasil) = temp;
+						} else if (opr == '/') {
+							op(tknHasil) = 'n';
+							temp = b / val(tknHasil);
+							val(tknHasil) = temp;
+						} else if (opr == '^') {
+							op(tknHasil) = 'n';
+							val(tknHasil) = pow(b, val(tknHasil));
+						} else {
+							printf("Token Salah!\n");
+						}
+					}
+					PushStack(&TknStck, tknHasil);
+					PushStack(&TknStck, currTkn);
+					lastPrecedence = getPrecedence(currTkn);
+				}
+			}
+		}
+		printf("lastPrecedence : %d\n", lastPrecedence);
+		printStack(TknStck);
+	} while (op(currTkn) != ')');
+}
 
 int main() {
 	CreateEmpty(&TknStck);
@@ -44,7 +135,22 @@ int main() {
 	do {
 		currTkn = readToken();
 		if (op(currTkn) == 'n') {
+			if (op(top(TknStck)) == 'n') {
+				b = val(PopStack(&TknStck));
+				val(currTkn) *= b;
+			}
 			PushStack(&TknStck, currTkn);
+		} else if (op(currTkn) == '(') {
+			PushStack(&TknStck, currTkn);
+			computeGan();
+			PopStack(&TknStck);
+			tknHasil = PopStack(&TknStck);
+			PopStack(&TknStck);
+			if (op(top(TknStck)) == 'n') {
+				b = val(PopStack(&TknStck));
+				val(tknHasil) *= b;
+			} 
+			PushStack(&TknStck, tknHasil);	
 		} else {
 			if ((getPrecedence(currTkn) > lastPrecedence) || (lastPrecedence == 3 && getPrecedence(currTkn) == 3)) {
 				PushStack(&TknStck, currTkn);
